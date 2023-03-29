41e986df4555961de16d06cda54ed38ab7f8e841
kv <kulvitall.vk@gmail.com>
2023-03-29 13:10:07 +0300

    add axios gettasks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,41 @@
 import React from "react";
 import './App.css';
+import axios from "axios";
+
+
+const DOMAIN = 'http://127.0.0.1:8000/api/todo/'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            todoList: [],
+            activeItem: {
+                id: null,
+                title: '',
+                user_name: '',
+                user_email: '',
+                completed: false
+            },
+            editing: false,
+        }
+        this.axiosTasks = this.axiosTasks.bind(this);
+    };
+
+    componentDidMount() {
+        this.axiosTasks()
+    }
+
+    axiosTasks() {
+        console.log('Connection...')
+
+        axios.get(DOMAIN)
+            .then(response => {
+                this.setState({todoList: response.data})
+            }).catch(error => console.log(error))
+
+    }
+
     render() {
         return (
             <div className="container">
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
 
 from todoapp.serializers import TaskModelSerializer
