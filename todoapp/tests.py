519277b240759be179a93068e62d0e4323077160
kv <kulvitall.vk@gmail.com>
2023-04-11 16:00:44 +0300

    added delete and edit tests

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -11,7 +11,7 @@ from .models import Task
 class TestTodoAppViewSet(TestCase):
     def test_get_list(self):
         factory = APIRequestFactory()
-        request = factory.get('/api/')
+        request = factory.get('/api/todo/')
         view = TaskModelViewSet.as_view({'get': 'list'})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -25,3 +25,45 @@ class TestTodoAppViewSet(TestCase):
         view = TaskModelViewSet.as_view({'post': 'create'})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_edit_task(self):
+        task = Task.objects.create(title='test task',
+                                   user_name="Test",
+                                   user_email='test@mail.ru')
+
+        client = APIClient()
+        response = client.put(f'/api/todo/{task.id}/', {"title": 'test task2'})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_edit_task_by_admin(self):
+        task = Task.objects.create(title='test task',
+                                   user_name="Test",
+                                   user_email='test@mail.ru')
+        client = APIClient()
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        client.login(username='admin', password='admin123456')
+        response = client.put(f'/api/todo/{task.id}/', {"title": 'test task2'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        task = Task.objects.get(id=task.id)
+        self.assertEqual(task.title, 'test task2')
+        client.logout()
+
+    def test_delete_task(self):
+        task = Task.objects.create(title='test task',
+                                   user_name="Test",
+                                   user_email='test@mail.ru')
+
+        client = APIClient()
+        response = client.delete(f'/api/todo/{task.id}/', {"title": 'test task2'})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_delete_task_by_admin(self):
+        task = Task.objects.create(title='test task',
+                                   user_name="Test",
+                                   user_email='test@mail.ru')
+        client = APIClient()
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', 'admin123456')
+        client.login(username='admin', password='admin123456')
+        response = client.delete(f'/api/todo/{task.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        client.logout()
