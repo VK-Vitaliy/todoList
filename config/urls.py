af5a04836d3f2e94907ec9847bef697869f3819c
kv <kulvitall.vk@gmail.com>
2023-04-11 16:37:39 +0300

    added swagger

--- config/urls.py
+++ config/urls.py
@@ -14,16 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path, include, re_path
+from rest_framework import routers, permissions
 from rest_framework.authtoken import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from todoapp.views import TaskModelViewSet
 
 router = routers.DefaultRouter()
 router.register('todo', TaskModelViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(title="Library", default_version='0.1', description="Documentation to out project",
+                 contact=openapi.Contact(email="kulvitall.vk@gmail.com"), license=openapi.License(name="MIT License"),
+                 ), public=True, permission_classes=[permissions.AllowAny], )
+
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
