ea43f48535bdd76c6994ce6a96b94faf9306e2d6
kv <kulvitall.vk@gmail.com>
2023-04-11 15:07:06 +0300

    added test_get_list, test_create_task

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -1,2 +1,27 @@
+import json
+from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
+from mixer.backend.django import mixer
+from django.contrib.auth.models import User
+from .views import TaskModelViewSet
+from .models import Task
 
-# Create your tests here.
+
+class TestTodoAppViewSet(TestCase):
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/')
+        view = TaskModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_task(self):
+        factory = APIRequestFactory()
+        request = factory.post('/api/', {'title': 'test task',
+                                         'user_name': "Test",
+                                         'user_email': 'test@mail.ru',
+                                         }, format='json')
+        view = TaskModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
